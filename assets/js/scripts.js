91b61557c2f2e603225569c8bb67d0c8f967b00e
Brian W <brianwalters952@gmail.com>
2021-05-06 21:14:45 -0500

    all sorts of changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,47 @@
-fetch(
-  "https://theaudiodb.p.rapidapi.com/searchtrack.php?s=bob%20marley&t=buffalo%20soldier",
-  {
-    method: "GET",
+
+
+let submitBtn = $("#submitBtn");
+
+
+function getToken(userGenre, userLength) {
+  console.log(userGenre);
+  const clientId = "4c2b7382b83842179f3780349cc8b6a6";
+  const clientSecret = "7a5ae8c0d13d415a9e477549ba0cfb7b";
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-      "x-rapidapi-key": "3cd0cafce3mshd3f737212ef361cp1924a4jsnda16ebd63191",
-      "x-rapidapi-host": "theaudiodb.p.rapidapi.com",
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
     },
-  }
-)
-  .then((response) => {
-    console.log(response);
+    body: "grant_type=client_credentials",
   })
-  .catch((err) => {
-    console.error(err);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      fetch(
+        `https://api.spotify.com/v1/recommendations?limit=${userLength}&market=ES&seed_genres=${userGenre}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Bearer  " + data.access_token,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    });
+}
+
+
+$(document).ready(function () {
+$("#submitBtn").click(function (event) {
+  event.preventDefault();
+  let userGenre = $("#inlineFormCustomSelectPrefone").val();
+  let userLength = $("#inlineFormCustomSelectPrefthree").val();
+  console.log($("#inlineFormCustomSelectPrefone").val());
+  getToken(userGenre, userLength);
+});
+})
 
